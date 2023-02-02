c14a7d6168662ef0fac08e38f5045f39f304d77b
sagadav <artursagadeev97@gmail.com>
2023-02-02 12:33:49 +0500

    deploy

---
+++ assets/about-view.a78b2f85.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as s}from"./index.2f834495.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function i(r,u){return t(),o("div",a,n)}const l=e(_,[["render",i]]);export{l as default};
